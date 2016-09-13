f1dc897f01909c16bdbff9e7384a163f89be38d7
Mellotainment <winstoncc56@gmail.com>
2016-09-13 09:55:01 -0400

    Add files via upload

---
+++ Assets/move.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public int playerX, playerZ;
+
+
+	// Use this for initialization
+	void Start () {
+
+		playerX = 0;
+		playerZ = 12;
+
+		transform.position= new Vector3 (playerX, 0, playerZ);
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		playerZ = (int)transform.position.z; 
+		playerX = (int)transform.position.x; 
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)){
+			transform.position += new Vector3 (0, 0, -1);
+
+			if (Input.GetKeyDown (KeyCode.DownArrow))
+				transform.position += new Vector3 (0, 0, 1);
+
+			if (Input.GetKeyDown (KeyCode.LeftArrow))
+				transform.position += new Vector3 (-1, 0, 0);
+
+			if (Input.GetKeyDown (KeyCode.RightArrow))
+				transform.position += new Vector3 (1, 0, 0);
+		}
+
+
+
+
+		if (playerX == 0&& playerZ == 2){ Debug.Log ("you reach the wall you Win");
+
+		}
+	}
+}
---
+++ Assets/nextlevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public  class nextlevel : MonoBehaviour {
+
+
+
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "player"){
+			
+			SceneManager.LoadScene("1");
+
+	}
+}
+} 
